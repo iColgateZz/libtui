66bb849348b5fb817c8ed6004eb5b3e817ebc004
pavelpavliv <ppavli@taltech.ee>
2026-02-08 13:38:25 +0200

    add build submodule

---
+++ main.c
@@ -0,0 +1,10 @@
+#define PSH_BUILD_IMPL
+    #include "psh_build/psh_build.h"
+
+i32 main(i32 argc, byte *argv[]) {
+    PSH_REBUILD_UNITY_AUTO(argc, argv);
+
+    printf("Hello, world!\n");
+
+    return 0;
+}
