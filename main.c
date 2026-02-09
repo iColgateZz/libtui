3a01cf2e8a8904a8cb96db316031ecff10fd30ee
pavelpavliv <ppavli@taltech.ee>
2026-02-09 14:41:18 +0200

    Move func decls to the top of the file, comment out mouse event escape codes

--- main.c
+++ main.c
@@ -7,27 +7,18 @@
 #include <assert.h>
 #include <ctype.h>
 
+void init_term();
+void restore_term();
+void write_str_len(byte *str, usize len);
+
+#define write_str(s)    write_str_len(s, sizeof(s) - 1)
+
 struct {
     struct termios orig_term;
 } Terminal = {0};
 
 void write_str_len(byte *str, usize len) {
     write(STDOUT_FILENO, str, len);
-}
-
-#define write_str(s)    write_str_len(s, sizeof(s) - 1)
-
-void restore_term() {
-    assert(tcsetattr(STDIN_FILENO, TCSAFLUSH, &Terminal.orig_term) == 0);
-
-    write_str("\33[?1000l");                     // disable mouse
-    write_str("\33[?1002l");                     // disable mouse
-    write_str("\33[m");                          // reset colors
-    write_str("\33[?25h");                       // show cursor
-    write_str("\33[?1049l");                     // exit alternate buffer
-
-    //TODO: If I print an error and the return to the original buffer,
-    // the error will not be visible to the user.
 }
 
 void init_term() {
@@ -44,12 +35,29 @@ void init_term() {
     write_str("\33[?2004l");                 // reset bracketed paste mode
     write_str("\33[?1049h");                 // use alternate buffer
     write_str("\33[?25l");                   // hide cursor
-    write_str("\33[?1000h");                 // enable mouse
-    write_str("\33[?1002h");                 // enable button events
+    // write_str("\33[?1000h");                 // enable mouse press/release
+    // write_str("\33[?1002h");                 // enable mouse press/release + drag
+    // write_str("\33[?1003h");                 // enable mouse press/release + drag + hover
+    // write_str("\33[?1006h");                 // use mouse sgr protocol
+    write_str("\33[0m");                     // reset text attributes
+    write_str("\33[2J");                     // clear screen
 
     assert(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == 0);
 
     atexit(restore_term);
+}
+
+void restore_term() {
+    assert(tcsetattr(STDIN_FILENO, TCSAFLUSH, &Terminal.orig_term) == 0);
+
+    // write_str("\33[?1000l");                     // disable mouse
+    // write_str("\33[?1002l");                     // disable mouse
+    write_str("\33[m");                          // reset colors
+    write_str("\33[?25h");                       // show cursor
+    write_str("\33[?1049l");                     // exit alternate buffer
+
+    //TODO: If I print an error and the return to the original buffer,
+    // the error will not be visible to the user.
 }
 
 i32 main(i32 argc, byte *argv[]) {
