57d081255ddde0a81e73e3f2781d5d397ee6da5c
pavelpavliv <ppavli@taltech.ee>
2026-02-12 22:19:35 +0200

    rm print_char function

--- main.c
+++ main.c
@@ -4,18 +4,6 @@
 #define PSH_BUILD_IMPL
     #include "psh_build/psh_build.h"
 
-#include <ctype.h>
-
-void print_char(Key k) {
-    if (k < 0) {
-        write_strf("Escaped sequence: %d\r\n", k);
-    } else if (k < 32) {
-        write_strf("Non-printable char: %d\r\n", k);
-    } else {
-        write_strf("Regular char: %c, %d\r\n", k, k);
-    }
-}
-
 typedef struct {
     Widget w;
     i32 state;
@@ -51,7 +39,7 @@ i32 main(i32 argc, byte *argv[]) {
 
     add_widget(&a);
 
-    while (1) {
+    while (!key_pressed('q')) {
         save_timestamp();
 
         // capture events
@@ -62,12 +50,6 @@ i32 main(i32 argc, byte *argv[]) {
             a.counter = 0;
             a.state = !a.state;
         }
-
-        // update state & other user logic
-        // Key k = get_key();
-        // print_char(k);
-        
-        if (key_pressed('q')) break;
         
         // draw ui
         draw_widgets();
