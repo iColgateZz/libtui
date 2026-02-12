183f37f7a30325b748054abe439d117db025b8ed
pavelpavliv <ppavli@taltech.ee>
2026-02-12 17:51:58 +0200

    Simple widget

--- main.c
+++ main.c
@@ -16,17 +16,52 @@ void print_char(Key k) {
     }
 }
 
+typedef struct {
+    Widget w;
+    i32 state;
+    u32 counter;
+} AnimationWidget;
+
+void draw(Widget *w) {
+    AnimationWidget *a = (AnimationWidget *)w;
+    if (a->state == 0) {
+        write_str("-");
+    } else {
+        write_str("|");
+    }
+}
+
 i32 main(i32 argc, byte *argv[]) {
     PSH_REBUILD_UNITY_AUTO(argc, argv);
 
     init_term();
     set_target_fps(60);
 
+    Widget w = {
+        .w = 10,
+        .h = 5,
+        .draw = draw,
+    };
+
+    AnimationWidget a = {
+        .w = w,
+        .state = 0,
+        .counter = 0,
+    };
+
+    add_widget(&a);
+
     while (1) {
         save_timestamp();
 
         // capture events
         poll_input();
+
+        a.counter += Terminal.dt;
+        if (a.counter > 1000) {
+            a.counter = 0;
+            a.state = !a.state;
+        }
 
         // update state & other user logic
         // Key k = get_key();
@@ -35,9 +70,10 @@ i32 main(i32 argc, byte *argv[]) {
         if (key_pressed('q')) break;
         
         // draw ui
+        draw_widgets();
 
         calculate_dt();
-        printf("dt: %llu\r\n", Terminal.dt);
+        // printf("dt: %llu\r\n", Terminal.dt);
     }
 
     return 0;
