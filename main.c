f06850d199f7ba4fe4f5e9cf749311f705c47d43
pavelpavliv <ppavli@taltech.ee>
2026-02-14 19:38:58 +0200

    Add a widget that auto fits itself to the middle of the screen

--- main.c
+++ main.c
@@ -4,28 +4,40 @@
 #define PSH_BUILD_IMPL
     #include "psh_build/psh_build.h"
 
-typedef struct Widget Widget;
-struct Widget {
+typedef struct {
     u32 x, y;
     u32 w, h;
-    void (*draw)(Widget *);
-};
-
-typedef struct {
-    Widget w;
     i32 state;
     u32 counter;
-} AnimationWidget;
+    u32 len;
+} Widget;
 
-void draw(Widget *w) {
-    AnimationWidget *a = (AnimationWidget *)w;
+void draw(Widget *a) {
+    u8 ch;
     if (a->state == 0) {
-        byte *text = "Some text!";
-        put_str(0, 0, text, strlen(text));
+        ch = '-';
     } else {
-        for (usize i = 0; i < 10; i++)
-            put_char(i, 0, '|');
+        ch = '|';
     }
+
+    for (usize i = 0; i < a->h; i++) {
+        for (usize j = 0; j < a->w; j++) {
+            put_char(a->x + j, a->y + i, ch);
+        }
+    }
+}
+
+void update(Widget *a) {
+    a->counter += get_delta_time();
+    if (a->counter > 1000) {
+        a->counter = 0;
+        a->state = !a->state;
+    }
+}
+
+void on_winch(Widget *a) {
+    a->x = get_terminal_width() / 2 - a->len / 2;
+    a->y = get_terminal_height() / 2 - a->len / 2;
 }
 
 i32 main(i32 argc, byte *argv[]) {
@@ -34,14 +46,13 @@ i32 main(i32 argc, byte *argv[]) {
     init_terminal();
     set_max_timeout_ms(16);
 
-    Widget w = {
-        .w = 10,
-        .h = 5,
-        .draw = draw,
-    };
-
-    AnimationWidget a = {
-        .w = w,
+    u32 len = 6;
+    Widget a = {
+        .x = get_terminal_width() / 2 - len / 2,
+        .y = get_terminal_height() / 2 - len / 2,
+        .w = len,
+        .h = len,
+        .len = len,
         .state = 0,
         .counter = 0,
     };
@@ -50,13 +61,12 @@ i32 main(i32 argc, byte *argv[]) {
         begin_frame();
 
         // application logic
-        a.counter += get_delta_time();
-        if (a.counter > 1000) {
-            a.counter = 0;
-            a.state = !a.state;
+        if (get_event_type() == EWinch) {
+            on_winch(&a);
         }
 
-        a.w.draw(&a.w);
+        update(&a);
+        draw(&a);
 
         end_frame();
     }
