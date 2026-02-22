46aa5ac6cb7f319846a6827af6a4b9af3fb123b9
pavelpavliv <ppavli@taltech.ee>
2026-02-22 18:54:44 +0200

    Add an example of using a non-ascii cp as termination key

--- main.c
+++ main.c
@@ -75,7 +75,7 @@ i32 main(i32 argc, byte *argv[]) {
     // push_scope(side - 1, 0, 1, 15);
     // push_scope(0, 0, 10, 10);
 
-    while (!is_codepoint(cp_from_byte('q'))) {
+    while (!is_codepoint(cp_new("э", 2, 1))) {
         begin_frame();
 
         // application logic
