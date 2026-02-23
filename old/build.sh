db6df788039a1874d4cfd85106d5a6e0cd17b701
pavelpavliv <ppavli@taltech.ee>
2026-02-23 14:01:22 +0200

    Remove the old files

--- old/build.sh
+++
@@ -1,1 +0,0 @@
-gcc -O2 -o app -Iinclude main.c terminal.c basis.c array.c screen.c widget.c window.c
--- old/terminal.h
+++
@@ -1,9 +0,0 @@
-#ifndef TERMINAL_HEADER
-#define TERMINAL_HEADER
-
-#include "types.h"
-
-void initTerminal(b32 disableEcho);
-
-#endif
-
