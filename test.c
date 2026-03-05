ec0018f36c163da4d2fd2637340edd7f9b6ed35e
pavelpavliv <ppavli@taltech.ee>
2026-03-05 17:37:04 +0200

    refactor a bit

--- test.c
+++ test.c
@@ -5,25 +5,22 @@
 
 #define SEC(x)  x * 1000
 
-typedef struct {
-    u16 timer;
-    Unix_Pipe pipe;
-} Asker;
-
 Cmd cmd = {0};
-Asker asker = {0};
+u16 timer = 0;
 Procs procs = {0};
 Fd_Reader reader;
 Sb buffer = {0};
 
 void main_loop();
+void parse_data();
+void reset_reader();
 
 i32 main(i32 argc, byte *argv[]) {
     PSH_REBUILD_UNITY_AUTO(argc, argv);
 
     init_terminal();
-    set_max_timeout_ms(100);
-    asker.timer = SEC(10);
+    set_max_timeout_ms(10);
+    timer = SEC(10);
     cmd_append(&cmd, "curl", "-s", "https://meowfacts.herokuapp.com/");
 
     while (!is_codepoint(cp("q"))) {
@@ -38,25 +35,21 @@ i32 main(i32 argc, byte *argv[]) {
 }
 
 void main_loop() {
-    asker.timer += get_delta_time();
-    if (asker.timer >= SEC(5)) {
-        asker.timer = 0;
+    timer += get_delta_time();
+    if (timer >= SEC(5)) {
+        timer = 0;
 
-        assert(pipe_open(&asker.pipe));
+        Unix_Pipe pipe;
+        assert(pipe_open(&pipe));
+        reader.fd = pipe.read_fd;
 
         // closes write fd
         cmd.count = 3;
-        cmd_run(
-            &cmd, 
-            .fdout = asker.pipe.write_fd,
-            .async = &procs
-        );
-
-        reader.fd = asker.pipe.read_fd;
+        cmd_run(&cmd, .fdout = pipe.write_fd, .async = &procs);
     }
 
-    // closes read fd when done reading
     if (reader.fd != INVALID_FD)
+        // closes read fd when done reading
         fd_read(&reader, .nonblocking = true);
 
     if (reader.ready) {
@@ -65,12 +58,21 @@ void main_loop() {
         psh__proc_wait_async(procs.items[0]);
         procs.count = 0;
 
-        reader.ready = false;
-        reader.fd = INVALID_FD;
-        buffer.count = 0;
-        sb_append_buf(&buffer, reader.store.items, reader.store.count);
-        reader.store.count = 0;
+        parse_data();
+        reset_reader();
     }
 
     put_ascii_str(0, 0, buffer.items, buffer.count);
+}
+
+void parse_data() {
+    // {"data":["text"]}
+    buffer.count = 0;
+    sb_append_buf(&buffer, reader.store.items, reader.store.count);
+}
+
+void reset_reader() {
+    reader.store.count = 0;
+    reader.ready = false;
+    reader.fd = INVALID_FD;
 }
