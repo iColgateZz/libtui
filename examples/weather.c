02b2b5951756632fb2b1f675b143230e65420af9
pavelpavliv <ppavli@taltech.ee>
2026-03-07 11:41:28 +0200

    mv s8 to psh_core

--- examples/weather.c
+++ examples/weather.c
@@ -5,20 +5,6 @@
 
 
 //TODO: mv s8 to psh_core
-#include <stddef.h>
-
-typedef struct {
-    byte *s;
-    usize len;
-} s8;
-
-#define countof(x)              (usize)(sizeof(x) / sizeof(*(x)))
-#define lenof(s)                countof(s) - 1
-
-#define s8(...)                       s8_(__VA_ARGS__, s8_from_heap, s8_read_only)(__VA_ARGS__)
-#define s8_(a, b, c, ...)             c
-#define s8_read_only(str)             (s8){.s = str, .len = lenof(str)}
-#define s8_from_heap(str, str_len)    (s8){.s = str, .len = str_len}
 
 void main_loop();
 void print_dimensions();
