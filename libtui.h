bf547146b8985b0cb0136e42c55ef7ae6c26115b
pavelpavliv <ppavli@taltech.ee>
2026-03-21 21:25:16 +0200

    -todo

--- libtui.h
+++ libtui.h
@@ -313,10 +313,6 @@ void restore_term() {
     write_str("\33[?25h");                       // show cursor
     write_str("\33[?1049l");                     // exit alternate buffer
 
-    //TODO: If I print an error and the return to the original buffer,
-    //      the error will not be visible to the user.
-    //      Global state error like errno?
-
     fd_close(Terminal.pipe.read_fd);
     fd_close(Terminal.pipe.write_fd);
 
