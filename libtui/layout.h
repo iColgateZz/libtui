a0fa260e57be5f2adc9f2f847302654b28f0a5af
pavelpavliv <ppavli@taltech.ee>
2026-05-03 12:13:10 +0300

    a sketch of layout engine

---
+++ libtui/layout.h
@@ -0,0 +1,42 @@
+#ifndef LIBTUI_LAYOUT_INCLUDE
+#define LIBTUI_LAYOUT_INCLUDE
+
+#define PSH_CORE_NO_PREFIX
+#include "psh_core/psh_core.h"
+
+typedef struct LayoutNode LayoutNode;
+list_def(LayoutNode);
+
+typedef enum {
+    LAYOUT_NODE_RECT,
+    LAYOUT_NODE_TEXT,
+} LayoutNodeType;
+
+struct LayoutNode {
+    LayoutNodeType type;
+    LayoutNode *parent;
+    List(LayoutNode) children;
+    // style properties
+};
+
+typedef enum {
+    LAYOUT_CMD_RECT,
+    LAYOUT_CMD_TEXT,
+    LAYOUT_CMD_CLIP,
+    LAYOUT_CMD_BORDER, // pass concrete codepoints to draw
+} LayoutCommandType;
+
+typedef struct {
+    LayoutCommandType type;
+    union {
+        // structs for commands
+    };
+} LayoutCommand;
+
+//TODO: add some api that either returns a list of cmds or lets user iterate over them
+
+#endif
+
+#ifdef LIBTUI_LAYOUT_IMPL
+
+#endif
