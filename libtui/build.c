73284b2f2e88681d691c5d77820f325c7530cd47
pavelpavliv <ppavli@taltech.ee>
2026-07-01 22:21:56 +0300

    test custom build script

---
+++ libtui/build.c
@@ -0,0 +1,33 @@
+#define PSH_CORE_IMPL
+#include "psh_core/psh_core.h"
+
+i32 main(i32 argc, byte *argv[]) {
+    PSH_REBUILD_UNITY_AUTO(argc, argv);
+    
+    #define BUILD_DIR "build"
+    Psh_Cmd cmd = {0};
+
+    if (argc > 1) {
+        byte *task = argv[1];
+        if (strncmp(task, "clean", 5) == 0) {
+            psh_cmd_append(&cmd, "rm", "-rf", BUILD_DIR);
+            if (!psh_cmd_run(&cmd)) exit(1);
+
+            return 0;
+        }
+    }
+
+    psh_cmd_append(&cmd, "mkdir", "-p", BUILD_DIR);
+    if (!psh_cmd_run(&cmd)) exit(1);
+
+    byte *paths[] = {
+        "main.c",
+        // "layout.h",
+        // "renderer.h"
+    };
+
+    psh_cmd_append(&cmd, PSH_CC_CMD(BUILD_DIR"/app", "main.c"));
+    if (!psh_cmd_run(&cmd)) exit(1);
+
+    return 0;
+}
