226f7119a852ac68c24de90258e2569420568a6c
pavelpavliv <ppavli@taltech.ee>
2026-07-03 17:05:07 +0300

    better error handling

--- libtui/build.c
+++ libtui/build.c
@@ -11,14 +11,17 @@ i32 main(i32 argc, byte *argv[]) {
         byte *task = argv[1];
         if (strncmp(task, "clean", 5) == 0) {
             psh_cmd_append(&cmd, "rm", "-rf", BUILD_DIR);
-            if (!psh_cmd_run(&cmd)) exit(1);
+            if (!psh_cmd_run(&cmd)) return 1;
 
             return 0;
         }
+
+        psh_logger(PSH_ERROR, "unknown command: %s", task);
+        return 1;
     }
 
     psh_cmd_append(&cmd, "mkdir", "-p", BUILD_DIR);
-    if (!psh_cmd_run(&cmd)) exit(1);
+    if (!psh_cmd_run(&cmd)) return 1;
 
     byte *paths[] = {
         "main.c",
@@ -27,7 +30,7 @@ i32 main(i32 argc, byte *argv[]) {
     };
 
     psh_cmd_append(&cmd, PSH_CC_CMD(BUILD_DIR"/app", "main.c"));
-    if (!psh_cmd_run(&cmd)) exit(1);
+    if (!psh_cmd_run(&cmd)) return 1;
 
     return 0;
 }
